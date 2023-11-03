b1d54a3390f6e635463e3762e7fed03e8fc0dcca
zhaoxiong.liang <zhaoxiong.liang@shopee.com>
2023-11-03 22:09:54 +0800

    fix: 更新 db

--- api/src/main.ts
+++ api/src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 9797;
 
   await app.listen(port, '0.0.0.0');
 
